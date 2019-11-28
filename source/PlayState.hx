0696ae11a8a5745e6496a5838966f79c43d9c141
isuke <isuke770@gmail.com>
2019-11-28 23:24:41 +0900

    init

---
+++ source/PlayState.hx
@@ -0,0 +1,15 @@
+package;
+
+import flixel.FlxState;
+import flixel.text.FlxText;
+
+class PlayState extends FlxState {
+  override public function create(): Void {
+    super.create();
+    add(new FlxText(10, 10, 100, "Hello, World!"));
+  }
+
+  override public function update(elapsed: Float): Void {
+    super.update(elapsed);
+  }
+}
