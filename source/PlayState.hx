145bf54b49b69cafa3ec18cd2315453559c4a3bc
isuke <isuke770@gmail.com>
2019-11-29 09:45:51 +0900

    implement move player

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,14 @@
 package;
 
 import flixel.FlxState;
-import flixel.text.FlxText;
+// import flixel.text.FlxText;
 
 class PlayState extends FlxState {
   override public function create():Void {
-    add(new FlxText(10, 10, 100, "Hello, World!"));
+    var _player:Player;
+    _player = new Player(20, 20);
+    add(_player);
+    // add(new FlxText(10, 10, 100, "Hello, World!"));
     super.create();
   }
 
